e25dc40367d6b1557da68f47f1562ddbf125a512
Cosme927 <67229630+Cosme927@users.noreply.github.com>
2021-06-12 17:50:36 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Build_OpenWrt_Orange_R1_Plus
+# 编译香橙派R1 Plus固件。
+# 1.默认IP：192.168.123.1
+# 2.待补充
---
+++ diy-part1_Lean.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git dockerman https://github.com/lisaac/luci-app-dockerman' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+
+#Add The Theme：luci-theme-opentomcat
+#git clone https://github.com/Cosme927/luci-theme-opentomcat package/luci-theme-opentomcat
