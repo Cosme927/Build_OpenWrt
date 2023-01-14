7c4e985433ce129ae886560dba787ccc1d686fd2
Cosme927 <67229630+Cosme927@users.noreply.github.com>
2023-01-14 19:45:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
