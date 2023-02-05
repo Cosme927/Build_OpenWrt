0eee2dbbd453789702b3891a0d2179c1f6d57f00
Cosme927 <67229630+Cosme927@users.noreply.github.com>
2023-02-05 12:13:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add feed source
 
 sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '$a src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go' feeds.conf.default
+#sed -i '$a src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go' feeds.conf.default
 
 #sed -i '$a src-git dockerman https://github.com/lisaac/luci-app-dockerman' feeds.conf.default
 #sed -i '$a src-git OpenAppFilter https://github.com/destan19/OpenAppFilter' feeds.conf.default
